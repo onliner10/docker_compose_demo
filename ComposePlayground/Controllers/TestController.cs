06fc5a76995a253a8bc4c847755c4aee76184d83
Mateusz Urban <mateuszurban2@gmail.com>
2019-11-03 13:39:25 +0100

    initial commit

---
+++ ComposePlayground/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+namespace ComposePlayground.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET: api/Test
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            var client = new ElasticClient(new Uri("http://elasticsearch:9200"));
+            var pingResponse = await client.PingAsync();
+            
+            return new
+            {
+                pingResponse.IsValid,
+                pingResponse.DebugInformation,
+            };
+        }
+    }
+}
